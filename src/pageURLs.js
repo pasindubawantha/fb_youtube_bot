1fa309d3e62b6336f5e59b853105ca84bb0db283
Pasindu Perera <pasindubawantha@gmail.com>
2017-07-11 21:49:48 +0530

    i

--- src/pageURLs.js
+++ src/pageURLs.js
@@ -1,7 +1,27 @@
 const pages = [
 	{
-		url :"hollydogeness",
-		parameters:{tags:['vines','funny'] , title:'funny vine', description:'funny vine', catogoryId:'24', minDescriptionLength:4, minTitleLength:4, maxTitleLength:255, maxVideoLength:900}
+		url :"https://www.facebook.com/Best-of-Instagram-and-Vine-Videos-427947250886112/",
+		parameters:{tags:['vine','funny','vines','instagram'] , title:'funny vine', description:'funny vine', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
+	},
+	{
+		url :"https://www.facebook.com/A-Bit-of-Everything-Videos-693558257494349/",
+		parameters:{tags:['vine','funny','vines'] , title:'funny vine', description:'funny vine', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
+	},
+	{
+		url :"https://www.facebook.com/A-Bit-of-Everything-Videos-693558257494349/",
+		parameters:{tags:['vine','funny','vines','cute','animals'] , title:'funny cute pets', description:'funny cute pets', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
+	},
+	{
+		url :"https://www.facebook.com/Pawsome-Animals-441815092831536/",
+		parameters:{tags:['vine','funny','vines','cute','animals'] , title:'funny cute pets', description:'funny cute pets', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
+	},
+	{
+		url :"https://www.facebook.com/pg/Awesome-Animal-Rescues-168501103626034/",
+		parameters:{tags:['vine','funny','vines','cute','animals','rescue'] , title:'Animal rescues', description:'Animal rescues', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
+	},
+	{
+		url :"OfficialFunnyVinesandVideos",
+		parameters:{tags:['vine','funny','vines'] , title:'funny vine', description:'funny vine', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
 	}
 ]
 
