79682530390b50d7f5a6a3904f97d83235249698
Pasindu Perera <pasindubawantha@gmail.com>
2017-07-13 18:59:38 +0530

    iii

--- src/pageURLs.js
+++ src/pageURLs.js
@@ -8,10 +8,6 @@ const pages = [
 		parameters:{tags:['vine','funny','vines'] , title:'funny vine', description:'funny vine', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
 	},
 	{
-		url :"https://www.facebook.com/A-Bit-of-Everything-Videos-693558257494349/",
-		parameters:{tags:['vine','funny','vines','cute','animals'] , title:'funny cute pets', description:'funny cute pets', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
-	},
-	{
 		url :"https://www.facebook.com/Pawsome-Animals-441815092831536/",
 		parameters:{tags:['vine','funny','vines','cute','animals'] , title:'funny cute pets', description:'funny cute pets', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
 	},
@@ -22,7 +18,32 @@ const pages = [
 	{
 		url :"OfficialFunnyVinesandVideos",
 		parameters:{tags:['vine','funny','vines'] , title:'funny vine', description:'funny vine', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
+	},
+	{
+		url :"DailyAnimalCutenessVideos",
+		parameters:{tags:['vine','funny','vines','cute','animals'] , title:'funny cute pets', description:'funny cute pets', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
+	},
+	{
+		url :"VeryDoge",
+		parameters:{tags:['vine','funny','vines','cute','animals','doge'] , title:'funny cute pets', description:'funny cute pets', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
+	},
+	{
+		url :"ShibeDoggo",
+		parameters:{tags:['vine','funny','vines','cute','animals','doge'] , title:'funny cute pets', description:'funny cute pets', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
+	},
+	{
+		url :"wittytrolls",
+		parameters:{tags:['vine','funny','vines','troll'] , title:'funny vine', description:'funny vine', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
+	},
+	{
+		url :"ViralBestVines",
+		parameters:{tags:['vine','funny','vines'] , title:'funny vine', description:'funny vine', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
+	},
+	{
+		url :"YerScottishVines",
+		parameters:{tags:['vine','funny','vines','scottish'] , title:'funny Scottish vine', description:'funny Scottish vine', catogoryId:'24', minDescriptionLength:6, minTitleLength:6, maxTitleLength:255, maxVideoLength:900}
 	}
+
 ]
 
 module.exports = pages
